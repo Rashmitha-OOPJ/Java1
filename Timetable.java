efa432556b2e220a7156934cb133321101d755f0
Rashmitha-OOPJ <rashmitha.23cs117@sode-edu.in>
2024-10-26 09:10:08 +0530

    Update and rename TestMyPoint.java to Timetable.java

---
+++ Timetable.java
@@ -0,0 +1,20 @@
+class Timetable{
+    String subject;
+    String faculty;
+    Timetable(String s,String f){
+        this.subject=s;
+        this.faculty=f;
+        
+    }
+    public void printtimetable(){
+        System.out.println("subject"+subject);
+        System.out.println("faculty"+faculty);
+        
+    }
+    public class timetable
+            public static void main(String[] args){
+                
+            }
+    
+}
+
