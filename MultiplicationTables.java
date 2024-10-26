d7e543b563c9829a509403341aadaf546800ca8d
Rashmitha-OOPJ <rashmitha.23cs117@sode-edu.in>
2024-10-26 10:23:54 +0530

    Add files via upload

---
+++ MultiplicationTables.java
@@ -0,0 +1,15 @@
+public class MultiplicationTables{
+    public static void main(String[] args){
+        for(int i=3;i<=8;i++){
+            System.out.println("Multiplication Table for "+1);
+            for(int j=1;j<=10;j++){
+                System.out.println(i+"x"+j+"="+(i*j));
+                System.out.println();
+            }
+        }
+    }
+                
+            
+        
+    
+    
