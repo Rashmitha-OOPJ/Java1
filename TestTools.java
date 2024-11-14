d3f579df9ef1421d945d822ad7b414ea1ecaec22
Rashmitha-OOPJ <rashmitha.23cs117@sode-edu.in>
2024-11-14 10:49:45 +0530

    TestTools.java

--- TestTools.java
+++ TestTools.java
@@ -3,10 +3,10 @@ public class TestTools {
     public static void main(String[] args){
         Tools t1=new Tools();
         t1.numbers(10);
-        t1.even(20);
-        t1.odd(19);
-        t1.mult3();
-        t1.mult4();
-        t1.mult5();
+        t1.even(10);
+        t1.odd(10);
+        t1.mult3(10);
+        t1.mult4(10);
+        t1.mult5(10);
     }
 }
