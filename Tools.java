8df3b446c3d918cbb12656d6f78c91c9371e3593
Rashmitha-OOPJ <rashmitha.23cs117@sode-edu.in>
2024-11-14 10:10:57 +0530

    Add files via upload

---
+++ Tools.java
@@ -0,0 +1,34 @@
+package pkg2;
+public class Tools{
+    public void numbers(int n1){
+        for(int i=0;i<=n1;i++){
+            System.out.print(i+" ");}
+        System.out.println();
+    }
+    public void even(int n){
+        for(int i=0;i<=n;i+=2){
+            System.out.print(i+" ");}
+        System.out.println();
+    }
+    public void odd(int n){
+        for(int i=1;i<=n;i+=2){
+            System.out.print(i+" ");}
+        System.out.println();}
+    public void mult3(){
+        for(int i=0;i<=30;i+=3){
+            System.out.print(i+" ");}
+        System.out.println();
+    }
+    public void mult4(){
+        for(int i=0;i<=32;i+=4){
+            System.out.print(i+" ");}
+        System.out.println();
+    }
+    public void mult5(){
+        for(int i=0;i<=30;i+=5){
+            System.out.print(i+" ");}
+        System.out.println();
+    }
+}
+
+
